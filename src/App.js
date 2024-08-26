3094153c8d08aecf1602d967122a669509597861
Bishal Chatterjee <deepcsr4@gmail.com>
2024-08-26 14:21:10 +0000

    Test Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Form.js'
+import Form from './components/Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Heading">This is a react app running on cloud</h1>
+      <Form></Form>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function Form() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert([inputs.username, inputs.age]);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <br></br>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <br></br>
+        <input type="submit" />
+    </form>
+  )
+}
+
+export default Form;
